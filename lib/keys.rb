9f0cb55b31a9e62b0c287faa730dbd21055d327b
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-19 20:59:30 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
-groceries = {
-  dairy: ["milk", "yogurt", "cheese"],
-  vegetable: ["carrots", "broccoli", "cucumbers"],
- meat: ["chicken", "steak", "salmon"],
- grains: ["rice", "pasta"]
+#groceries = {
+ # dairy: ["milk", "yogurt", "cheese"],
+ # vegetable: ["carrots", "broccoli", "cucumbers"],
+# meat: ["chicken", "steak", "salmon"],
+# grains: ["rice", "pasta"]
 }
 
 def get_the_keys(groceries)
